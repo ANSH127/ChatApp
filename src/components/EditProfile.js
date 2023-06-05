dd8994f8565c6d69243b148b0966f882666b13a0
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-06-06 00:11:19 +0530

    updating file

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -8,19 +8,23 @@ import { useNavigate } from 'react-router-dom'
 import EditIcon from '@mui/icons-material/Edit';
 import Button from '@mui/material/Button';
 import { ToastContainer, toast } from 'react-toastify';
-
+import DoneIcon from '@mui/icons-material/Done';
+import ClearIcon from '@mui/icons-material/Clear';
 function EditProfile(props) {
     const navigate = useNavigate()
     const [details, setDetails] = useState({ name: '', email: '' })
     const [edit, setEdit] = useState(false)
     const [edit2, setEdit2] = useState(false)
+    const [isusername, setIsusername] = useState(true)
+    const [currentuser, setCurrentuser] = useState(null)  // eslint-disable-next-line
 
     useEffect(() => {
         const fetchUser = async () => {
             const { data: { user } } = await supabase.auth.getUser()
             if (user) {
                 console.log(user);
-                setDetails({ name: user.user_metadata.username, email: user.email })
+                setDetails({ name: user.user_metadata.username, email: user.email,id:user.id })
+                setCurrentuser(user.user_metadata.username)
             }
             else {
                 navigate('/login');
@@ -45,7 +49,38 @@ function EditProfile(props) {
         setDetails({ ...details, name: '' });
         setEdit2(true);
     }
+    const checkusername = async (username) => {
+        if (username.length < 4) {
+            setIsusername(false);
+            return;
+        }
+        const { data, error } = await supabase
+            .from('Users')
+            .select('username')
+            .ilike('username', username)
+        if (error) {
+            toast.error('Something went wrong')
+            console.log(error)
+            return;
+        }
+        if (data.length > 0) {
+            if (data[0].username===currentuser) {
+                setIsusername(true);
+                return;
+            }
+            setIsusername(false);
+            return;
+        }
+        else {
+            setIsusername(true);
+        }
+
+    }
     const updateMail = async () => {
+        if (details.email.length < 1) {
+            toast.error('Email cannot be empty')
+            return;
+        }
 
         const { data, error } = await supabase.auth.updateUser({ email: details.email })
         if (error) {
@@ -78,10 +113,25 @@ function EditProfile(props) {
             setEdit2(false);
             // console.log(data);
             document.getElementById('username').disabled = true;
+            updateUserProfile();
 
 
         }
 
+
+    }
+
+    const updateUserProfile = async () => {
+        const {error} =await supabase
+        .from('Users')
+        .update({username:details.name})
+        .eq('user_id',details.id)
+        if(error){
+            toast.error('Something went wrong')
+            console.log(error)
+            return;
+        }
+        
 
     }
 
@@ -103,17 +153,32 @@ function EditProfile(props) {
                     disabled
                     style={props.mode === 'dark' ? { marginBottom: 20, marginTop: 20, display: 'block', backgroundColor: '#fff', color: '#fff' } : { marginBottom: 20, marginTop: 20, display: 'block' }
                     }
-                    onChange={(e) => { setDetails({ ...details, name: e.target.value }) }}
+                    onChange={(e) => {
+                         setDetails({ ...details, name: e.target.value })
+                         checkusername(e.target.value)
+
+                         }}
                     InputProps={{
                         endAdornment: (
                             <>
-
+                                
+                                {isusername && <DoneIcon
+                                    sx={{ cursor: 'pointer', marginRight: 2,color:'green' }}
+                                    
+                                />}
 
                                 {!edit2 && <EditIcon
                                     sx={{ cursor: 'pointer', marginRight: 2 }}
                                     onClick={usernameedit}
                                 />}
-                                {edit2 && <Button
+                                {!isusername && <ClearIcon
+                                    sx={{ cursor: 'pointer', marginRight: 2,color:'red' }}
+
+                                />}
+
+                                {edit2 && isusername && 
+                                currentuser!==details.name && 
+                                 <Button
                                     variant="outlined"
                                     color="primary"
                                     size="small"
